72d68110ca78a09dac8e496d3d84bdaa36ab1a49
Chirakid Kaweesirikon <chirakid.k@gmail.com>
2019-04-24 20:32:19 +0700

    update file

--- src/ListTodo/Item.js
+++ src/ListTodo/Item.js
@@ -1,36 +1,38 @@
 import React from 'react';
 import { ListGroupItem, InputGroup, InputGroupAddon, InputGroupText, Button, Input } from 'reactstrap';
 
-const Item = (props) => {
-    return (
-        <ListGroupItem>
+class Item extends React.Component {
+    render() {
+        return (
+            <ListGroupItem>
 
+                <InputGroup>
+                    <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                            <Input
+                                addon
+                                type="checkbox"
+                                defaultChecked={this.props.isCompleted}
+                                onChange={() => this.props.onToggleComplete(this.props.index)}
+                            />
+                        </InputGroupText>
+                    </InputGroupAddon>
 
-            <InputGroup>
-                <InputGroupAddon addonType="prepend">
-                    <InputGroupText>
-                        <Input
-                            addon
-                            type="checkbox"
-                            defaultChecked={props.isCompleted}
-                            onChange={() => props.onToggleComplete(props.index)}
-                        />
-                    </InputGroupText>
-                </InputGroupAddon>
+                    <Input
+                        type="input"
+                        disabled={this.state.isDisabled}
+                        value={this.props.todoName}
+                        onChange={(event) => this.props.onEditTodo(this.props.index, event.target.value)}
+                    />
 
-                <Input
-                    type="input"
-                    value={props.todoName}
-                    onChange={(event) => props.onEditTodo(props.index, event.target.value)}
-                />
+                    <InputGroupAddon addonType="append">
+                        <Button size="sm" onClick={() => this.props.onDeleteTodo(this.props.index)} color="danger"><i className="far fa-trash-alt"></i></Button>
+                    </InputGroupAddon>
+                </InputGroup>
 
-                <InputGroupAddon addonType="append">
-                    <Button size="sm" onClick={() => props.onDeleteTodo(props.index)} color="danger"><i className="far fa-trash-alt"></i></Button>
-                </InputGroupAddon>
-            </InputGroup>
-
-        </ListGroupItem>
-    )
-};
+            </ListGroupItem>
+        )
+    }
+}
 
 export default Item;
